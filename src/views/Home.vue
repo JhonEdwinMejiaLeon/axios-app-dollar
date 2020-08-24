ce40b5cf3f364a34f9a21db12b798a81a7d9c8e6
JhonEdwinMejiaLeon <em97759@gmail.com>
2020-08-24 18:51:51 -0500

    esta es la version final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,64 @@
 <template>
   <div>
     <v-layout :wrap = " true ">
-      <v-flex xs6 md6>
-        <h1>Hola mundo!</h1>
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker 
+          v-model="fecha" 
+          full-width 
+          locale="es-co"
+          :min="minimo"
+          :max="maximo"
+          @change="getDollar(fecha)"
+          ></v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            $ Precio Dolar Chile : {{valor}}
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </div>
 </template>
 
 <script>
+  import axios from "axios";
+  import {mapMutations} from "vuex";
   export default {
-  
+    data() {
+      return {
+        fecha: new Date().toISOString().substr(0, 10),
+        minimo: '1984',
+        maximo: new Date().toISOString().substr(0, 10),
+        valor: null
+      }
+    },
+    methods: {
+      ...mapMutations(['mostrarLoading','ocultarLoading']),
+
+      async getDollar(dia){
+        let ddmmyy = dia.split('-').reverse().join('-');
+
+        try{
+          this.mostrarLoading({titulo:'Accediendo a informacion', color:'secondary'})
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+
+          if (datos.data.serie.length > 0){
+             this.valor = await datos.data.serie[0].valor;
+          }else {
+             this.valor = 'Sin resultado'
+          }
+        }catch (e){
+          console.log(e)
+        }finally{
+          this.ocultarLoading()
+        }
+
+      }
+    },
+    created(){
+      this.getDollar(this.fecha)
+    }
   }
 </script>
