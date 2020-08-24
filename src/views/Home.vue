4e1c1e8170d186e49066ee8e77b1fe1a804f6715
JhonEdwinMejiaLeon <em97759@gmail.com>
2020-08-24 15:52:57 -0500

    esta es la version numero 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-layout :wrap = " true ">
+      <v-flex xs6 md6>
+        <h1>Hola mundo!</h1>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+  
   }
-}
 </script>
